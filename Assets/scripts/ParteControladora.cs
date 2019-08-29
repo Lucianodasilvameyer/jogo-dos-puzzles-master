e6b664ef8330f1fe9b5a5a6b12442a1c9d9fcb01
Lucianodasilvameyer <murcilhax@hotmail.com>
2019-08-28 22:39:35 -0300

    inicio no puzzle 9 partes e fazer portal

---
+++ Assets/scripts/ParteControladora.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParteControladora : MonoBehaviour
+{
+
+    int[] Position = new int[9];
+    
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+   
+}
